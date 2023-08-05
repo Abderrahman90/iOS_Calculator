1239989040d574a51a7034a5c99f458cd9689744
Abderrahman90 <abderrahman90@gmail.com>
2023-08-05 11:06:46 +0200

    ok

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CalculatriceIOS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
